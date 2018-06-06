2da6351a27a8f750c5347dfd63be49b1e0c2125d
Otimt <ddd-1-ddd@163.com>
2018-06-06 15:43:52 +0800

    增加音频伴随+pdf播放功能

--- src/assets/vue/components/study/audio.vue
+++ src/assets/vue/components/study/audio.vue
@@ -1,17 +1,116 @@
 <script>
     export default {
         data(){
-            return {};
+            return {
+                //固定参数
+                playbackSpeedIndex:2,//倍速
+                speedList:[0.5,0.75,1,1.5,2],
+                pdfInterval:null,
+                pdfFrameDoc:null,
+                pdfCurrent:1,
+                pdfTotal:0,
+                mp3:null,
+
+                //动态加载数据
+                title:"欢迎来到课工场",
+                audioUrl:"/iframe/mp3/adiao.mp3",//视频地址
+                time2pdf:[
+                    0,30,60,90,120,150,180,210,240,270,300
+                ],
+
+            };
         },
-        created () {
+        mounted () {
+            localStorage.setItem("pdfjs.history","");//清除 pdf 播放记录，自动回到第一页
+            this.mp3 = document.getElementById("course-audio");
+            this.pdfInterval = setInterval(function(){
+                if(this.pdfTotal == 0){
+                    this.pdfTotal = this.getPdfTotal();
+                    console.log("正在加载pdf总数");
+                }else{
+                    var time2pdf = this.time2pdf;
+                    for(var i=0,il=time2pdf.length;i<il;i++){
+                        if(this.mp3.currentTime >= time2pdf[i] &&  (i+1)<time2pdf.length && this.mp3.currentTime < time2pdf[i+1]){
+                            let page = i+1;
+                            console.log("去第"+page+"页");
+                            if(this.pdfCurrent !== page){
+                                console.log("真去第"+page+"页");
+                                this.pdfGoto(page);
+                            }
+                            break;
+                        }
+                    }
+                }
+            }.bind(this),1000)
+        },
+        beforeDestroy(){
+            clearInterval(this.pdfInterval);
         },
         methods: {
+            setPdfFrameDoc(){
+                this.pdfFrameDoc = document.getElementById("pdfFrame").contentWindow.document;
+            },
+            getPdfTotal(){
+                let numPagesDom = this.pdfFrameDoc && this.pdfFrameDoc.getElementById("numPages");
+                if(!numPagesDom){
+                    return 0;
+                }else{
+                    let total = this.pdfFrameDoc.getElementById("numPages").innerHTML;
+                    console.log("total"+total);
+                    return Number(total.match(/\d+/)[0])
+                }
+            },
+            pdfGoto(num){
+                let input = this.pdfFrameDoc.getElementById("pageNumber");
+                this.pdfCurrent = input.value=num;
+                input.trigger("change");
+            },
+            pdfPrev(){
+                this.pdfFrameDoc.getElementById('previous').trigger("click");
+            },
+            pdfNext(){
+                this.pdfFrameDoc.getElementById('next').trigger("click");
+            },
+            speedUp(){
+                if(this.playbackSpeedIndex<(this.speedList.length-1)){
+                    this.playbackSpeedIndex++;
+                    var myVid=document.getElementById("course-audio");
+                    myVid.playbackRate=this.speedList[this.playbackSpeedIndex];
+                }
+            },
+            speedDown(){
+                if(this.playbackSpeedIndex>0){
+                    this.playbackSpeedIndex--;
+                    var myVid=document.getElementById("course-audio");
+                    myVid.playbackRate=this.speedList[this.playbackSpeedIndex];
+                }
+            },
         }
     }
 </script>
 <style>
-
+    .w700{width:700px;}
+    .course-pdf{
+        position:relative;
+        width:700px;
+        height:360px;
+        background:#000;
+    }
+    .speed-box{
+        position: absolute;
+        top:0;
+        right:0;
+        width:90px;
+        height:20px;
+    }
 </style>
 <template>
-    <div>音频</div>
+    <div class="w h">
+        <div>{{title}}</div>
+        <div class="course-pdf">
+            <iframe @load="setPdfFrameDoc" id="pdfFrame" src="/iframe/pdf/web/viewer.html" class="w h" frameborder="0"></iframe>
+            <div class="speed-box white ar pr5"><span @click="speedDown" class="cur-p">«</span> {{speedList[playbackSpeedIndex]}}×倍速 <span @click="speedUp" class="cur-p">»</span></div>
+        </div>
+        <audio id="course-audio" :src="audioUrl" class="w700" controls></audio>
+    </div>
 </template>
--- src/assets/vue/components/study/homework.vue
+++ src/assets/vue/components/study/homework.vue
@@ -2,6 +2,15 @@
     export default {
         data(){
             return {
+                fileList: [
+//                    {
+//                        name: 'food.jpeg',
+//                        url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+//                    }, {
+//                        name: 'food2.jpeg',
+//                        url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'
+//                    }
+                ],
                 directions:"<p class='t2em'>富文本</p><p class='t2em'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet. Proin gravida dolor sit amet lacus accumsan et viverra justo commodo. Proin sodales pulvinar tempor. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nam fermentum, nulla luctus pharetra vulputate, felis tellus mollis orci, sed rhoncus sapien nunc eget.</p>",
                 status:"not_uploaded",//not_uploaded | uploaded | passed | failed
                 score:"",
@@ -29,6 +38,19 @@
                     default:
                         return "未上传作业。"
                 }
+            },
+            //element组件抄过来 http://element-cn.eleme.io/#/zh-CN/component/upload
+            handleRemove(file, fileList) {
+                console.log(file, fileList);
+            },
+            handlePreview(file) {
+                console.log(file);
+            },
+            handleExceed(files, fileList) {
+                this.$message.warning(`当前限制选择 3 个文件，本次选择了 ${files.length} 个文件，共选择了 ${files.length + fileList.length} 个文件`);
+            },
+            beforeRemove(file, fileList) {
+                return this.$confirm(`确定移除 ${ file.name }？`);
             }
         }
     }
@@ -43,8 +65,22 @@
 
         <div class="mt20">状态：{{ statusText() }}</div>
         <div class="mt20 ac">
-            <el-button type="primary" v-if="status==='not_uploaded'">上传作业</el-button>
-            <el-button type="primary" v-else-if="status==='not_pass'|| status==='uploaded'">再次上传</el-button>
+            <!--multiple-->
+            <el-upload
+                    class="upload-demo"
+                    action="https://jsonplaceholder.typicode.com/posts/"
+                    :on-preview="handlePreview"
+                    :on-remove="handleRemove"
+                    :before-remove="beforeRemove"
+                    :limit="1"
+                    :on-exceed="handleExceed"
+                    :file-list="fileList">
+                <el-button size="small" type="primary">
+                    <template v-if="status==='not_uploaded'">上传作业</template>
+                    <template v-else-if="status==='not_pass'|| status==='uploaded'">再次上传</template>
+                </el-button>
+                <div slot="tip" class="el-upload__tip">上传文件不能超过500kb</div>
+            </el-upload>
         </div>
 
 
